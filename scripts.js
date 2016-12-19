21aafe5dac0e8d91ab9d37e1986ea459fe5cad3d
SurlyMae <surlymae@gmail.com>
2016-12-19 15:00:32 -0700

    Complete first exercise

--- scripts.js
+++ scripts.js
@@ -1,3 +1,23 @@
-//Use this file to implement Part One of your project
+'use strict';
 
+const animal = {}
 
+animal.username = 'Daffy'
+
+animal['tagline'] = 'I like ducks.'
+
+var noises = []
+
+animal.noises = noises
+
+console.log('Animal: ', animal)
+
+var count = 0
+for (var key in animal) {
+  count++;
+  if (key === 'username') {
+    console.log(`Hey, my name is ${animal[key]}.`)
+  } else if (key === 'tagline') {
+    console.log(`I say ${animal[key]}`)
+  }
+}
